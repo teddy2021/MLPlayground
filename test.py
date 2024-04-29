4c1432a46168d945d0ced055e6481cd86fb6d431
teddy2021 <darkking@live.ca>
2024-04-29 11:10:31 -0400

    Fixed issue causing the optimizaition step to fail. Added in flash attention and checkpointing.

--- test.py
+++ test.py
@@ -2,43 +2,31 @@ import sys
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-torch.autograd.set_detect_anomaly(True)
+import torch.utils.checkpoint as checkpoint
 device = 'cpu'#'cuda' if torch.cuda.is_available() else 'cpu'
-from tokenizers import Tokenizer
-from tokenizers import decoders
+torch.autograd.set_detect_anomaly(True)
 import matplotlib.pyplot as plt
 
-class Block(nn.Module):
-	def __init__(self, embeddings, headcount, context_size):
-		super().__init__()
-		head_size = embeddings // headcount
-		self.sa = MultiHeadAttention(headcount, head_size, embeddings, context_size, 0.2)
-		self.ffwd = FeedForward(embeddings, 0.2)
+words = []
+toid = {}
+tostr = {}
+def encode(wordset):
+	out = []
+	for word in wordset:
+		encoding = toid[word]
+		out.append(encoding)
+	return out
 
-		self.ln1 = nn.LayerNorm(embeddings)
-		self.ln2 = nn.LayerNorm(embeddings)
+def decode(encoding):
+	out = []
+	for num in encoding:
+		word = tostr[num]
+		out.append(word)
+	return out
 
-	def forward(self, x):
-		y = x.clone().detach()
-		y =+ self.sa(self.ln1(x))
-		y += self.ffwd(self.ln2(x))
-		return y
-
-class MultiHeadAttention(nn.Module):
-	def __init__(self, headcount, headsize, embeddings, context_size, d_chance):
-		super().__init__()
-		self.heads= nn.ModuleList([Head(embeddings, headsize, context_size, d_chance) for _ in range(headcount)])
-		self.proj = nn.Linear(headsize * headcount, embeddings)
-		self.dropout = nn.Dropout(d_chance)
-
-	def forward(self,x):
-		out = torch.cat([h(x) for h in self.heads], dim=-1)
-		out = self.proj(out)
-		out = self.dropout(out)
-		return out
 
 class Head(nn.Module):
-	def __init__(self, dims, head_sz, size, d_chance):
+	def __init__(self, dims, head_sz, size, d_chance=0.2):
 		super().__init__()
 		self.key = nn.Linear(dims, head_sz, bias=False)
 		self.query = nn.Linear(dims, head_sz, bias=False)
@@ -51,18 +39,50 @@ class Head(nn.Module):
 		B, T, C = x.shape
 		key = self.key(x)
 		query = self.query(x)
-
-
-		weight = query @ key.transpose(-2, -1) * (key.shape[-1] ** -0.5)
-		weight = weight.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-		weight = self.dropout(F.softmax(weight, dim=-1))
-
 		value = self.value(x)
-		out = weight @ value
+
+		#weight = query @ key.transpose(-2, -1) * (key.shape[-1] ** -0.5)
+		#weight = weight.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+		#weight = self.dropout(F.softmax(weight, dim=-1))
+
+		
+		out = F.scaled_dot_product_attention(query, key, value) #weight @ value
+		
 		return out
 
+class MultiHeadAttention(nn.Module):
+	def __init__(self, headcount, headsize, embeddings, context_size, d_chance=0.2):
+		super().__init__()
+		self.heads= nn.ModuleList([Head(embeddings, headsize, context_size, d_chance) for _ in range(headcount)])
+		self.proj = nn.Linear(headsize * headcount, embeddings)
+		self.dropout = nn.Dropout(d_chance)
+
+	def forward(self,x):
+		out = torch.cat([h(x) for h in self.heads], dim=-1)
+		out = checkpoint.checkpoint(self.proj, out, use_reentrant=False)
+		out = self.dropout(out)
+		return out
+
+
+class Block(nn.Module):
+	def __init__(self, embeddings, headcount, context_size):
+		super().__init__()
+		head_size = embeddings // headcount
+		self.sa = MultiHeadAttention(headcount, head_size, embeddings, context_size, 0.2)
+		self.ffwd = FeedForward(embeddings, 0.2)
+
+		self.ln1 = nn.LayerNorm(embeddings)
+		self.ln2 = nn.LayerNorm(embeddings)
+
+	def forward(self, x):
+		x = x + self.sa(self.ln1(x))
+		t = checkpoint.checkpoint(self.ln2, x, use_reentrant=False)
+		x = x + self.ffwd(self.ln2(t))
+		return x
+
+
 class FeedForward(nn.Module):
-	def __init__(self, embeddings, d_chance):
+	def __init__(self, embeddings, d_chance=0.2):
 		super().__init__()
 		self.net = nn.Sequential(
 			nn.Linear(embeddings, 4*embeddings),
@@ -74,11 +94,14 @@ class FeedForward(nn.Module):
 		return self.net(x)
 
 class Model(nn.Module):
-	def __init__(self, context_size, batche_count, vocabulary_size, embed_dims, headcount, block_count):
+	def __init__(self, context_size, batche_count, vocabulary_size, embed_dims, headcount, block_count=4):
 		super().__init__()
 		self.context_size = context_size
 		self.batch_count = batche_count
 		self.dims = embed_dims
+		self.vocab_size = vocabulary_size
+		self.n_heads = headcount
+		self.n_blocks = block_count
 
 		self.token_embedding_table = nn.Embedding(vocabulary_size, embed_dims)
 		self.position_embedding_table = nn.Embedding(context_size, embed_dims)
@@ -109,7 +132,6 @@ class Model(nn.Module):
 		tok_pos = self.blocks(tok_pos)
 		tok_pos = self.layer_norm(tok_pos)
 		logits = self.lm_head(tok_pos)
-
 		if None == targets:
 			loss = None
 		else:
@@ -147,25 +169,31 @@ class Model(nn.Module):
 if __name__ == '__main__':
 	print('Beginning...')
 	print('Running on',device)
-	token = Tokenizer.from_file('Plato-BPE.json')
-	token.decoder = decoders.BPEDecoder()
-	with open('Plato Complete Works_djvu.txt', 'r', encoding='utf-8') as f:
+	with open('input.txt', 'r', encoding='utf-8') as f:
 		text = f.read()
-	print(f'Read text [{len(text)}]')
+	print(f'Read text [{len(text)}]')	
+	print("Encoding data")
+
+	raw = text.split()
+	print(f'{len(raw)} total tokens')
+	words = list(set(raw))
+	toid = {wrd:i for i, wrd in enumerate(words)}
+	tostr = {i:wrd for i, wrd in enumerate(words)}
 	
-	print("encoding data")
-	ids = list(set([token.encode(wrd).ids[0] for wrd in text.split()]))
-	data = torch.tensor([token.encode(wrd).ids[0] for wrd in text.split()], dtype=torch.long)
-	sz = int(len(data) * 0.9)
-	training = data[:sz]
-	validation = data[sz:]
+	data = torch.tensor(encode(raw), dtype=torch.long)
+	length = len(data)
+	print(f'{length} data points in total')
+	training = data[:int(length * 0.9) ]
+	print(f'\t{len(training)} training data points')
+	validation = data[int(length * 0.9):]
+	print(f'\t{len(validation)} validation data points')
 
 
-	batch_count = 100
-	size = 256 #context
+	batch_count = 64
+	size = 2048 if len(words) > 512 else len(words) // 4 #context
 	head_count = 8
-	dims = 384
-	iters = 5000
+	dims = 32
+	iters = 1000
 	learn = 3e-4
 	block_count = 8
 	loss_interval = iters // 20
@@ -174,7 +202,7 @@ if __name__ == '__main__':
 	print('Setup variables and lambdas.')
 
 
-	m = Model(size, batch_count, token.get_vocab_size(), dims, head_count, block_count)
+	m = Model(size, batch_count, len(words), dims, head_count, block_count)
 	m = m.to(device)
 
 
@@ -183,25 +211,27 @@ if __name__ == '__main__':
 
 	print(f'Setup model and optimization {sum(p.numel() for p in m.parameters())/1e6} M parameters')
 	for steps in range(iters):
-		print(f'Training...\t{100*steps/iters}% \tGetting batch',end='\r')
 		if(steps % loss_interval == 0):
+			print(f'\rTraining...\t{100*steps/iters}% \tEvaluating loss sample', end='')
 			x.append(steps//loss_interval)
-			y.append(m.estimate_loss(100, training))
+			y.append(m.estimate_loss(20, training))
+		print(f'\rTraining...\t{100*steps/iters}% \tGetting batch', end='')
 		batch_in, batch_out = m.get_batch(training)
-		print(f'Training...\t{100*steps/iters}% \tForwarding through batch',end='\r')
-		_, loss = m(batch_in, batch_out)
-
-		print(f'Training...\t{100*steps/iters}% \tOptimizing',' ' * 32, end='\r')
+		print(f'\rTraining...\t{100*steps/iters}% \tForwarding through batch', end='')
+		output, loss = m(batch_in, batch_out)
+		print(f'\rTraining...\t{100*steps/iters}% \tOptimizing',' ' * 32, end='')
+		
 		optimizer.zero_grad(set_to_none=True)
 		loss.backward()
 		optimizer.step()
+		
 
 	print(f'Training...\t 100%',' ' *32)
 	con = torch.zeros((1,1), dtype=torch.long, device=device)
-	pred = m.predict(con, maximal_tokens=500)
-	pred = pred[0].tolist()
-	print(f'\n\nWith a loss of {m.estimate_loss(300, validation)} we have {token.decode(pred)}')
-	m.save(model.state_dict(), 'model.ml')
+	pred = m.predict(con, maximal_tokens=500)[0].tolist()
+	print(f'\n\nWith a loss of {m.estimate_loss(300, validation)} we have {" ".join(decode(pred))}')
 	fig, ax = plt.subplots()
 	ax.plot(x, y)
 	fig.show()
+	m.save(model.state_dict(), 'model.ml')
+
